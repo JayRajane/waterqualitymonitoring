82f32cee3f2ff764477a8ad12fb33715d2819d00
Jay Rajane <159191778+JayRajane@users.noreply.github.com>
2025-06-04 14:42:27 +0530

    update

--- water_monitoring_system/water_monitoring_system/settings.py
+++ water_monitoring_system/water_monitoring_system/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.102','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.104','localhost','127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
