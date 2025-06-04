c55fb1913ff2b2de79b5945dd255add756d84d7c
Akash <akash@aapkapainter.com>
2025-06-04 20:01:46 +0530

    allowed hosts

--- water_monitoring_system/settings.py
+++ water_monitoring_system/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.102','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
